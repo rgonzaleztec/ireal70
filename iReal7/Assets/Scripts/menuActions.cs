81bc9e4a8e566cb00713ee35afb92986d187e749
Rogelio Gonzalez <rogelio.rgonzaez@gmail.com>
2022-05-20 15:35:11 -0600

    menu y principal

---
+++ iReal7/Assets/Scripts/menuActions.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuActions : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public AudioSource audiogeneral;
+
+    public void QuitApplication ()
+    {
+        Application.Quit();
+    }
+
+    public void LaunchScene1()
+    {
+        //Agregar una animacion de cambio de escena
+        SceneManager.LoadScene(1);
+    }
+
+    public void LaunchScene2()
+    {
+        //Agregar una animacion de cambio de escena
+        SceneManager.LoadScene(2);
+    }
+
+    public void MuteAudio()
+    {
+        if (audiogeneral.isPlaying)
+        {
+            audiogeneral.Stop();
+        }
+        else
+        {
+            audiogeneral.Play();
+        }
+    }
+
+
+    
+}
